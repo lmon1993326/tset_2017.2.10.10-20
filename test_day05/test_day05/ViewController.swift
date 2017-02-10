24dbd7e6a3793107aff1a081eb2330fd9706bd4d
lmon1993326 <270216150@qq.com>
2017-02-10 10:18:54 +0800

    Today is Feb.10.2017
    I test GitHub code is publish to there

---
+++ test_day05/test_day05/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test_day05
+//
+//  Created by nyato喵特 on 2017/2/10.
+//  Copyright © 2017年 nyato喵特. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
